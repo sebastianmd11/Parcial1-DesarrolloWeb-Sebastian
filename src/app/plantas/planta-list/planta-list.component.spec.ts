40f1947dc9761072f711b06f8d03e696800e7397
Sebastian Mascareno Dagnino <s.mascareno@uniandes.edu.co>
2023-04-29 10:49:59 -0700

    Se realizan las pruebas al componente planta-list

--- src/app/plantas/planta-list/planta-list.component.spec.ts
+++ src/app/plantas/planta-list/planta-list.component.spec.ts
@@ -4,13 +4,18 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { PlantaListComponent } from './planta-list.component';
+import { Planta } from '../planta';
+import { faker } from '@faker-js/faker';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('PlantaListComponent', () => {
   let component: PlantaListComponent;
   let fixture: ComponentFixture<PlantaListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [ PlantaListComponent ]
     })
     .compileComponents();
@@ -19,10 +24,42 @@ describe('PlantaListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PlantaListComponent);
     component = fixture.componentInstance;
+
+    const plantas: Planta[] = [];
+
+    let tipo = ["Exterior", "Interior"];
+
+
+    for (let i = 0; i<3; i++){
+      let eleccion = tipo[Math.floor(Math.random() * 2)];
+      const planta = new Planta(
+        i+1,
+        faker.name.fullName(),
+        faker.name.fullName(),
+        eleccion,
+        faker.datatype.number({max:150}),
+        faker.lorem.word(),
+        faker.lorem.word()
+      );
+        component.plantas.push(planta);
+    }
+
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have 1 <tr class="header"> element', () => {
+    expect(debug.queryAll(By.css('tr.header'))).toHaveSize(1)
+  });
+
+  it('should have 4 <tr> elements', () => {
+    fixture.detectChanges(); // Actualiza la vista para que la tabla sea visible
+    const tableRows = fixture.debugElement.queryAll(By.css('tr')); // Obtiene todos los elementos 'tr' de la tabla
+    expect(tableRows.length).toBe(4); // Verifica que la tabla tenga exactamente 3 elementos 'tr'
   });
 });
